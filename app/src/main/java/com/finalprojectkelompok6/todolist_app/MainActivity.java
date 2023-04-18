322fac25b83eebccc6f7504af40dce4f1ec60b57
MDaffaIzzulhaqS <izzulhaqdaffasuyanto@gmail.com>
2023-04-19 05:21:51 +0700

    Menambahkan Bottom Navigation Bar

--- app/src/main/java/com/finalprojectkelompok6/todolist_app/MainActivity.java
+++ app/src/main/java/com/finalprojectkelompok6/todolist_app/MainActivity.java
@@ -1,14 +1,49 @@
 package com.finalprojectkelompok6.todolist_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.finalprojectkelompok6.todolist_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.about:
+                    replaceFragment(new AboutFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+                case R.id.exit:
+                    System.exit(0);
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
