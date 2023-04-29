903b62e7ba98904bf1e40bd6961b3e1286b61b8e
Rendi Septia Rahman <rendiseptia287@gmail.com>
2023-04-29 11:28:29 +0700

    Menampilkan Task di MainActivity

--- app/src/main/java/com/finalprojectkelompok6/todolist_app/MainActivity.java
+++ app/src/main/java/com/finalprojectkelompok6/todolist_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.finalprojectkelompok6.todolist_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,13 +8,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 
+import com.finalprojectkelompok6.todolist_app.Adapter.ToDoAdapter;
+import com.finalprojectkelompok6.todolist_app.Model.ToDoModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private FloatingActionButton mFab;
+    private FirebaseFirestore firestore;
+    private ToDoAdapter adapter;
+    private List<ToDoModel> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recycler_view);
         mFab = findViewById(R.id.floating_action_button);
+        firestore = FirebaseFirestore.getInstance();
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -30,6 +45,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AddNewTask.newInstance().show(getSupportFragmentManager() , AddNewTask.TAG);
+            }
+        });
+
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(MainActivity.this , mList);
+
+        recyclerView.setAdapter(adapter);
+        showData();
+    }
+
+    private void showData(){
+        firestore.collection("task").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                for (DocumentChange documentChange : value.getDocumentChanges()){
+                    if(documentChange.getType() == DocumentChange.Type.ADDED){
+                        String id = documentChange.getDocument().getId();
+                        ToDoModel toDoModel = documentChange.getDocument().toObject(ToDoModel.class).withId(id);
+                        mList.add(toDoModel);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+                Collections.reverse(mList);
             }
         });
     }
