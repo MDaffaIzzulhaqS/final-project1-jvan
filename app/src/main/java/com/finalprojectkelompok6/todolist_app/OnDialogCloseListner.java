057445cdea3156d2b8657d92a24aad76f6a87a4c
Rendi Septia Rahman <rendiseptia287@gmail.com>
2023-04-29 10:56:35 +0700

    Menambahkan Add New Task

---
+++ app/src/main/java/com/finalprojectkelompok6/todolist_app/OnDialogCloseListner.java
@@ -0,0 +1,7 @@
+package com.finalprojectkelompok6.todolist_app;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+    void onDialogClose(DialogInterface dialogInterface);
+}
